a96c02a0010af7377abc202f4b4e91eec5639250
Berrones Edwin <Berrones.Edwin@hotmail.com>
2022-05-24 12:49:20 -0500

    feat: component for meta tags

---
+++ components/Head.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export  const HeadComponent = () => {
+  return (
+    <Head>
+      <title>Weather App</title>
+      <meta name="description" content="weather App using API weatherAPI" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
+
+
