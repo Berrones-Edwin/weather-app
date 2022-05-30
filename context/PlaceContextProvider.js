8b7d74ca9e1a8f3680c1fdbbb822abb8283037fa
Berrones Edwin <Berrones.Edwin@hotmail.com>
2022-05-30 12:37:43 -0500

    fix: add effect in context

--- context/PlaceContextProvider.js
+++ context/PlaceContextProvider.js
@@ -1,10 +1,19 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
+import { getData } from '../utils/localstorage'
 
 export const PlaceContext = createContext(null)
 
 const PlaceContextProvider = ({ children }) => {
   const [place, setPlace] = useState('')
-  return <PlaceContext.Provider value={{ place, setPlace }}>{children}</PlaceContext.Provider>
+
+  useEffect(() => {
+    setPlace(getData('searches'))
+  }, [setPlace])
+  return (
+    <PlaceContext.Provider value={{ place, setPlace }}>
+      {children}
+    </PlaceContext.Provider>
+  )
 }
 
 export default PlaceContextProvider
